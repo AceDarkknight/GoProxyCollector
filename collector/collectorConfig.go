17f9ed13adfa72d92fccb2c31829e49a97c3dc22
Dilon Wu <acewudilin@foxmail.com>
2018-03-20 23:17:43 +0800

    update collector to read config from xml

--- collector/collectorConfig.go
+++ collector/collectorConfig.go
@@ -1,13 +1,35 @@
 package collector
 
+import "os"
+
+//github.com/antchfx/xmlquery
+//github.com/beevik/etree
+//type Configs struct {
+//	Config []struct {
+//	} `xml:"Configs>Config"`
+//}
+
 type Config struct {
-	Name          string
-	urlFormat     string
-	urlParameters []string
+	Name          string   `xml:"name,attr"`
+	urlFormat     string   `xml:"urlFormat"`
+	urlParameters []string `xml:"urlParameters"`
 	currentUrl    string
 	currentIndex  int
 	ValueRuleMap  map[string]string
 	Type          CollectType
+}
+
+type ConfigXml struct {
+	Name          string   `xml:"name,attr"`
+	UrlFormat     string   `xml:"urlFormat"`
+	UrlParameters []string `xml:"urlParameters"`
+	ValueRuleMap  struct {
+		Items []struct {
+			Name string `xml:"name,attr"`
+			Path string `xml:"path,attr"`
+			Attr string `xml:"attr"`
+		} `xml:"item"`
+	} `xml:"valueNamePathMap"`
 }
 
 type CollectType uint8
@@ -18,5 +40,10 @@ const (
 )
 
 func NewCollectorConfig() *Config {
+	file, err := os.Open("config.xml")
+	if err != nil {
+		panic(err)
+	}
 
+	defer file.Close()
 }
